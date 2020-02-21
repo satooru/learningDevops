857e60dbf9732335e31c67488e9da041997fb05a
satooru <16670493+satooru@users.noreply.github.com>
2020-02-21 02:58:09 -0300

    added simple smoke test

--- jasonSays/src/main/java/com/satooru/jasonSays/JasonSaysApplication.java
+++ jasonSays/src/main/java/com/satooru/jasonSays/JasonSaysApplication.java
@@ -1,13 +1,28 @@
 package com.satooru.jasonSays;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.satooru.jasonSays.model.Quote;
+import com.satooru.jasonSays.repository.interfaces.QuoteRepository;
+
 @SpringBootApplication
-public class JasonSaysApplication {
+public class JasonSaysApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JasonSaysApplication.class, args);
-	}
+    @Autowired
+    private QuoteRepository repository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(JasonSaysApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.deleteAll();
+        System.out.println(repository.count());
+        repository.save(new Quote(1, "Just Do it!"));
+        System.out.println(repository.count());
+    }
 }
