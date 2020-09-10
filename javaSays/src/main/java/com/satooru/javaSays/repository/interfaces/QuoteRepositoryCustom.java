229c7a4336f156e2e7d98962d460e196de712f5e
satooru <16670493+satooru@users.noreply.github.com>
2020-09-10 19:45:08 -0300

    removed javaSays as part of this repo

--- javaSays/src/main/java/com/satooru/javaSays/repository/interfaces/QuoteRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.satooru.javaSays.repository.interfaces;
-
-import com.satooru.javaSays.model.Quote;
-
-public interface QuoteRepositoryCustom {
-    Quote findRandomQuote();
-    Quote findLatestQuote();
-    void delete(String quote);
-}
